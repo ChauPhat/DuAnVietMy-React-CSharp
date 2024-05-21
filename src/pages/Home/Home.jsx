fea0c67fdb0d134456398909e189fcf1f8e7f582
ChauPhat <hieuphung2111@gmail.com>
2024-05-21 18:18:28 +0700

    chauphat: done Create Food List component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExloreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
     <div>Home
       <Header/>
       <ExloreMenu  category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
