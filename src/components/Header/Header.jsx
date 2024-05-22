897139d9228dfe55fa4398a457c421f0a644f401
ChauPhat <hieuphung2111@gmail.com>
2024-05-22 13:22:35 +0700

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () =>{
     return (
         <div className='header'>
             <div className="header-contents">
-                <h2>Order your facourite food here</h2>
+                <h2>Order your favourite food here</h2>
                 <p>Chose from a diverse menu fearuting a delectable array of dishes crafted with the finest ingredients and culinary expertse. Our mission is to satisfy your cravings and elevate your dining experience one. one delicious meal at a time</p>
                 <button>View Menu</button>
             </div>
